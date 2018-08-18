d446f86729c255925511950a136692439b03d174
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 13:59:35 -0300

    Import and inject colorsType filter

--- src/filters/index.js
+++ src/filters/index.js
@@ -1,7 +1,9 @@
-import Vue from 'vue';
+import Vue from "vue";
 
-import stringReplace from './stringReplace.filter';
+import stringReplace from "./stringReplace.filter";
+import colorsType from "./colorsTypes.filter";
 
-Vue.filter('replace', stringReplace);
+Vue.filter("replace", stringReplace);
+Vue.filter("type-color", colorsType);
 
 export default Vue;
