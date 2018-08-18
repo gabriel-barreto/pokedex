cc787996c5c4042ef0091be20e1e93e7d00f67ed
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 14:49:52 -0300

    Integrate with Sidebar component service

--- src/components/shared/Navbar/Navbar.vue
+++ src/components/shared/Navbar/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
     <nav>
         <div class="navbar">
-            <button class="btn btn-sidebar-toggler">
+            <button class="btn btn-sidebar-toggler" @click="openSidebar">
                 <span class="icon"></span>
             </button>
             <router-link :to="{ name: 'home' }">
@@ -12,46 +12,58 @@
 </template>
 
 <script>
-    export default {
-        name: 'Navbar'
-    }
+// ==> Components services
+import Sidebar from '../Sidebar';
+
+export default {
+    name: 'Navbar',
+    methods: {
+        openSidebar () {
+            Sidebar.show();
+        },
+    },
+}
 </script>
 
 <style lang="scss">
-    .navbar {
-        align-items: center;
-        background-color: #e74c3c;
-        box-shadow: 0 4px 6px 0 rgba(#000, .2);
-        display: flex;
-        padding: 1rem 2.5rem;
-        position: fixed;
-        width: 100%;
-        .btn {
-            margin-right: .5rem;
-            .icon {
-                position: relative;
-                &, &::after, &::before {
-                    background-color: #fff;
-                    display: block;
-                    height: .3rem;
-                    width: 2.25rem;
-                }
-                &::before, &::after {
-                    content: '';
-                    position: absolute;
-                }
-                &::before {
-                    top: -7px;
-                }
-                &::after {
-                    bottom: -7px;
-                }
+.navbar {
+    align-items: center;
+    background-color: #e74c3c;
+    box-shadow: 0 4px 6px 0 rgba(#000, 0.2);
+    display: flex;
+    padding: 1rem 2.5rem;
+    position: fixed;
+    width: 100%;
+    .btn {
+        height: 3.3rem;
+        margin-right: 0.5rem;
+        .icon {
+            position: relative;
+            &,
+            &::after,
+            &::before {
+                background-color: #fff;
+                display: block;
+                height: 0.3rem;
+                width: 2.25rem;
+            }
+            &::before,
+            &::after {
+                content: "";
+                position: absolute;
+            }
+            &::before {
+                top: -7px;
+            }
+            &::after {
+                bottom: -7px;
             }
         }
-        .navbar-brand {
-            max-width: 100%;
-            height: 3.5rem;
-            width: auto;
-        }
     }
+    .navbar-brand {
+        max-width: 100%;
+        height: 3.5rem;
+        width: auto;
+    }
+}
 </style>
