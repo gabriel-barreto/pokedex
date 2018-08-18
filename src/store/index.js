a97d190ec5e8632d582df12de148aa1b46cd5020
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 14:00:05 -0300

    Remove unused props

--- src/store/index.js
+++ src/store/index.js
@@ -1,27 +1,18 @@
-import Vue from 'vue';
-import Vuex from 'vuex';
+import Vue from "vue";
+import Vuex from "vuex";
 
 Vue.use(Vuex);
 
 const store = new Vuex.Store({
     state: {
-        types: [],
-        pokemons: [],
+        pokemons: []
     },
 
     mutations: {
-        registerPokemon(state, payload) {
-            console.log(payload);
+        add(state, payload) {
             state.pokemons = payload.content;
-            // const types = new Set();
-            // state.pokemons.map(pokemon => {
-            //     pokemon.types.map(type => {
-            //         types.add(type);
-            //     });
-            // });
-            // state.types = [...types];
-        },
-    },
+        }
+    }
 });
 
 export default store;
