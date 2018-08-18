0833d5a033b16e6d70232535c3557e8571cc877e
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 14:05:13 -0300

    Create fetchPokes action to download all pokes from API

--- src/store/index.js
+++ src/store/index.js
@@ -1,8 +1,14 @@
+// ==> Importing dependencies
 import Vue from "vue";
 import Vuex from "vuex";
 
+// ==> Importing services
+import service from "../service/pokemon.service";
+
+// ==> Injecting Vue plugins
 Vue.use(Vuex);
 
+// ==> Create new store
 const store = new Vuex.Store({
     state: {
         pokemons: []
@@ -10,7 +16,21 @@ const store = new Vuex.Store({
 
     mutations: {
         add(state, payload) {
-            state.pokemons = payload.content;
+            state.pokemons = payload;
+        }
+    },
+
+    actions: {
+        getList({ commit }) {
+            const pokeServices = new service();
+            pokeServices
+                .list()
+                .then(pokes => {
+                    commit("add", pokes);
+                })
+                .catch(err => {
+                    console.log(err);
+                });
         }
     }
 });
