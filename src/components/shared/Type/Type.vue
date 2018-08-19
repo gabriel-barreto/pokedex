a3aca5012a9e826458ad83946a839fa0aa517e87
Gabriel Barreto <barreto-gabriel@outlook.com>
2018-08-18 21:47:14 -0300

    Use colorsType filter to get bg color

--- src/components/shared/Type/Type.vue
+++ src/components/shared/Type/Type.vue
@@ -1,30 +1,38 @@
 <template>
-    <div
-        class="type"
-        :style="color"
-    >{{ label }}</div>
+    <div class="type" :style="typeBg" @click="applyFilter(label)">{{ label }}</div>
 </template>
 
 <script>
-    export default {
-        name: 'Type',
-        props: {
-            color: '',
-            label: '',
+export default {
+    name: 'Type',
+    props: {
+        label: '',
+    },
+    computed: {
+        typeBg () {
+            const color = this.$options.filters.typeColor(this.label);
+            return `background-color: ${color};`;
         },
-    };
+    },
+    methods: {
+        applyFilter (type) {
+            console.log(type);
+        },
+    },
+};
 </script>
 
 <style lang="scss">
-    .type {
-        border-radius: 2px;
-        box-shadow: 0 2px 4px 0 rgba(#000, .2);
-        color: #fff;
-        font-family: Helvetica, Arial, sans-serif;
-        font-size: 1.4rem;
-        font-weight: bold;
-        letter-spacing: .1rem;
-        padding: .75rem 1rem;
-        text-transform: uppercase;
-    }
+.type {
+    border-radius: 2px;
+    box-shadow: 0 2px 4px 0 rgba(#000, 0.2);
+    color: #fff;
+    font-family: Helvetica, Arial, sans-serif;
+    font-size: 1.4rem;
+    font-weight: bold;
+    letter-spacing: 0.1rem;
+    padding: 1rem 1.25rem;
+    text-transform: uppercase;
+    width: fit-content;
+}
 </style>
